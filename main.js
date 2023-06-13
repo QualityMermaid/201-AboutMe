54bae863d33fb30667e7de59539d7f060988ac78
JessicaBane <jessicambane@gmail.com>
2023-06-13 20:09:10 +0100

    got to an ok spot and need to save

--- main.js
+++ main.js
@@ -16,49 +16,83 @@ function yourName(){
     console.log(user)
 
 }
-
+let husbandName
+let fatherName
+let degreeIn
+let hobby
+let job
+let ready //working
 function guessGame(){
-    let husbandName
-    let fatherName
-    let degreeIn
-    let hobby
-    let job
+    // let husbandName
+    // let fatherName
+    // let degreeIn
+    // let hobby
+    // let job
+    // let ready
 
     console.log(user)
     const response = confirm("Are you ready " + user + "?")
     console.log(response)
     if (response){
         alert("OK lets start! Please answer y/yes or n/no");
-        
-        husbandName = prompt("Is my husband called Nick?").toLowerCase()
-        while(husbandName != "yes" && husbandName != "no" && husbandName != "n" && husbandName != "y"){
-            alert("Please only answer in yes/no or y/n ");
-            husbandName = prompt("Is my husband called Nick? Please answer yes/no or y/n only").toLowerCase();
-        
-                if(husbandName === "y" || husbandName === "yes" ){
-                    console.log("Correct his name is Nick")
-                    alert("Thats correct!")
-            } else if (husbandName === "no" || husbandName === "n"){ 
-                console.log("Not write answer") ;
-                alert("Sorry " + user + " that's not the right answer.")
-            }
-                else {console.log("Why are you not working")}
-            }
-        }
-        fatherName = prompt("Is Nick the name of my father?").toLowerCase()
-        while(fatherName != "yes" && fatherName != "no" && fatherName != "n" && fatherName != "y"){
-            alert("Please only answer in yes/no or y/n ");
-            fatherName = prompt("Is Nick the name of my father?").toLowerCase();
-        
-                if(fatherName === "y" || fatherName === "yes" ){
-                    console.log("Correct his name is Nick")
-                    alert("Thats correct!")
-            } else if (fatherName === "no" || fatherName === "n"){ 
-                console.log("Not write answer") ;
-                alert("Sorry " + user + " that's not the right answer.")
-            }
-                else {console.log("Why are you not working")}
-        } 
+        console.log("Ready to do this")
+    // return ready=true;}
     //     else { alert("Come back when your ready.");
+    //     console.log("notready")
+    //     return ready=false
+    //     }
     // }
-}
+// function guessGameAgain(){
+    // console.log(ready + "again")
+    if(ready === true){
+        alert("Here we go!");
+    //     // console.log("im ready")
+        guessGameAgain();}
+    else {alert("something isnt write")}
+// }
+return ready=true;}
+        else { alert("Come back when your ready.");
+        console.log("notready")
+        return ready=false
+        }
+    }
+
+    // function guessGameAgain(){
+    //     console.log("Did this work?")
+    // }
+
+
+
+
+
+    // } // working to here
+// } // working to here
+        
+        // husbandName = prompt("Is my husband called Nick?").toLowerCase()
+        // while(husbandName != "yes" && husbandName != "no" && husbandName != "n" && husbandName != "y"){
+        //     alert("Please only answer in yes/no or y/n ");
+        //     husbandName = prompt("Is my husband called Nick? Please answer yes/no or y/n only").toLowerCase();
+        
+        //         if(husbandName === "y" || husbandName === "yes" ){
+        //             console.log("Correct his name is Nick")
+        //             alert("Thats correct!")
+        //     } else if (husbandName === "no" || husbandName === "n"){ 
+        //         console.log("Not write answer") ;
+        //         alert("Sorry " + user + " that's not the right answer.")
+        //     }
+        //         else {console.log("Why are you not working")}
+        //     }
+        // }
+        // fatherName = prompt("Is Nick the name of my father?").toLowerCase()
+        // while(fatherName != "yes" && fatherName != "no" && fatherName != "n" && fatherName != "y"){
+        //     alert("Please only answer in yes/no or y/n ");
+        //     fatherName = prompt("Is Nick the name of my father?").toLowerCase();
+        
+        //         if(fatherName === "y" || fatherName === "yes" ){
+        //             console.log("Correct his name is Nick")
+        //             alert("Thats correct!")
+        //     } else if (fatherName === "no" || fatherName === "n"){ 
+        //         console.log("Not write answer") ;
+        //         alert("Sorry " + user + " that's not the right answer.")
+        //     }
+
