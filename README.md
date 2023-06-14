f1a033d130d358b5fb2ede79f19e74afb3fb12f5
JessicaBane <jessicambane@gmail.com>
2023-06-14 13:36:13 +0100

    saving current notes of class 02 and 03 live demo

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # 201-AboutMe
 AboutMe page created during 201 class which will be built upon
+13th June:
+- added quiz on main page that asks 5 Y/N questions
+- added logic around null and empty user values
+- added logic for skipping question (alert cancel)
 
+14th June adding more functionality
+- Added top 10
+- Work and Education into unordered list
+- added 6th and 7th question
+- added final score card
+- 
 
 <!--  Make sure to add font-style awesome for if user == Christ -->
--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 alert("Hi, my name is Jessica, Welcome to my site!")
 let user = yourName();
-console.log(user)
+let points = 0;
+let wrongPoints = 0;
+let skippedQuestion = 0;
+console.log("users name =" + user)
 
 function yourName(){
     let user
@@ -32,11 +35,11 @@ function guessGame(){
         ready = true
         alert("Please only answer in yes/no or y/n ")
 
-        testQuiz("Is my husband's name Nick?", "y", "Correct "+ user )
-        testQuiz("Is my dad called Nick?", "y", "Yes his name is also Nick. Well done "+ user )
-        testQuiz("Do I have a BA in Fine Arts?", "n", "Correct "+ user + ", I have a BA in Digital Animation" )
-        testQuiz("Am I a Divemaster?", "n", "Good spot "+ user + ". I am a Master Scuba Diver not Divermaster")
-        testQuiz("Test/Quality Engineers break code?", "n", "Correct "+user+". Testing doesn't break code. It just shows you it's broken!")
+        testQuiz("Is my husband's name Nick?", "y", "n", "Correct "+ user, "Sorry that's incorrect " + user )
+        testQuiz("Is my dad called Nick?", "y", "n", "Yes his name is also Nick. Well done "+ user , "Sorry that's incorrect " + user )
+        testQuiz("Do I have a BA in Fine Arts?", "n", "y", "Correct "+ user + ", I have a BA in Digital Animation", "Sorry that's incorrect " + user  )
+        testQuiz("Am I a Divemaster?", "n", "y", "Good spot "+ user + ". I am a Master Scuba Diver not Divermaster", "Sorry that's incorrect " + user )
+        testQuiz("Test/Quality Engineers break code?", "n", "y", "Correct "+user+". Testing doesn't break code. It just shows you it's broken!", "Sorry that's incorrect " + user )
         testQuizFinished()
         
     if(ready === true){
@@ -51,26 +54,42 @@ function guessGame(){
     }
 
 // // testQuiz("What is the date", "y", "Correct "+ user )
-function testQuiz(question, answer, message){
+function testQuiz(question, answer, wrongAnswer, messageCorrect, messageWrong){
     let userAnswer
     while(userAnswer != "yes" && userAnswer != "no" && userAnswer != "n" && userAnswer != "y"){
-        console.log(userAnswer);
+        console.log("This is an answer?" + userAnswer);
         userAnswer = prompt(question);
         if(userAnswer == null){
             console.log(userAnswer)
             alert("OK fine don't answer this question?")
+            skippedQuestion = skippedQuestion + 1
+            console.log("Currenlt no. of skipped questions " + skippedQuestion)
             return
         } else {
             console.log(userAnswer)
             userAnswer = userAnswer.toLowerCase()
         }
     
-            if((userAnswer === "y" || userAnswer === "yes") && answer === "y" ){
-                console.log("Correct his name is Nick")
-                alert(message)
-        } else if ((userAnswer === "no" || userAnswer === "n") && answer === "n"){ 
-            console.log("Not write answer") ;
-            alert(message)
+        if((userAnswer === "y" || userAnswer === "yes") && answer === "y" ){
+            console.log("Correct answer1")
+            alert(messageCorrect)
+            points = points+1
+            console.log("current points are "+ points + "wrong points " + wrongPoints)
+        } else if((userAnswer === "n" || userAnswer === "no") && answer === "n" ){
+            console.log("Correct answer2")
+            alert(messageCorrect)
+            points = points+1
+            console.log("current points are "+ points + "wrong points " + wrongPoints)
+        } else if((userAnswer === "y" || userAnswer === "yes") && answer === "n" ){
+            console.log("Sorry not right1")
+            alert(messageWrong)
+            wrongPoints = wrongPoints+1
+            console.log("current points are "+ points + "wrong points " + wrongPoints)
+        }  else if((userAnswer === "n" || userAnswer === "no") && answer === "y" ){
+            console.log("Sorry not right2")
+            alert(messageWrong)
+            wrongPoints = wrongPoints+1
+            console.log("current points are "+ points + "wrong points " + wrongPoints)
         } else if(userAnswer != "yes" && userAnswer != "no" && userAnswer != "n" && userAnswer != "y"){
             console.log(userAnswer)
             alert("Please check you input is in y/n yes/no format.")
@@ -79,12 +98,13 @@ function testQuiz(question, answer, message){
             alert("OK " + user + " hope you will come back later.")
         }
         else {
-            alert("Sorry " + user + " that's not the right answer.");
+            alert("Sorry " + user + " something went wrong. Try again");
+
             console.log("Why are you not working")
         }
     }
 }
 
 function testQuizFinished(){
-    alert("Thank you " + user + " for taking part of my quiz.")
+    alert("Thank you " + user + " for taking part of my quiz. You have answer " + points + " questions correctly! You also skipped " + skippedQuestion + ".")
 }
